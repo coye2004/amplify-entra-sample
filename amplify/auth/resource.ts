c82286139130d5b0ff3e415900ec46ebc70c80e9
Haiyun Xu <Haiyun.Xu@autodesk.com>
2025-09-16 20:08:24 +0800

    simpiler the codes for testing

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,25 +1,20 @@
 import { defineAuth, secret } from '@aws-amplify/backend';
 
 export const auth = defineAuth({
-  // 1) What users can log in with (local Cognito + external IdPs)
   loginWith: {
-    email: true,
     externalProviders: {
-      callbackUrls: ['http://localhost:5174/','https://main.d2j5lex9wj9ikc.amplifyapp.com'],
-      logoutUrls: ['http://localhost:5174/','https://main.d2j5lex9wj9ikc.amplifyapp.com'],
+      // required at this level
+      callbackUrls: ['http://localhost:5174/','https://main.d2j5lex9wj9ikc.amplifyapp.com/'],
+      logoutUrls: ['http://localhost:5174/','https://main.d2j5lex9wj9ikc.amplifyapp.com/'],
+
+      // OIDC IdP definition
       oidc: [
         {
           name: 'AutodeskEntra',
           issuerUrl: 'https://login.microsoftonline.com/87b99afb-a707-40ce-9edb-59da9c64c857/v2.0',
-          clientId: secret('AUTODESK_ENTRA_ID_CLIENT_ID'),      // must be 77c7…
+          clientId: secret('AUTODESK_ENTRA_ID_CLIENT_ID'),
           clientSecret: secret('AUTODESK_ENTRA_ID_CLIENT_SECRET'),
           scopes: ['openid', 'profile', 'email'],
-          attributeMapping: {
-            email: 'email',
-            preferredUsername: 'preferred_username',
-            givenName: 'given_name',
-            familyName: 'family_name',
-          },
         },
       ],
     },
